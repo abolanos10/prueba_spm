80aacc8db28aeadbb3494e5ffb6f1e154cf76625
Adrián Bolaños Ríos <abolanos@plainconcepts.com>
2023-01-19 15:04:30 +0100

    prueba

--- Package.swift
+++ Package.swift
@@ -20,7 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FrameworkSPM",
-            dependencies: []),
+            dependencies: [],
+        resources: [
+            .process("Frameworks/MobilePaymentSDK.xcframework"),
+            .copy("MobilePaymentSDK.xcframework")
+        ]),
         .binaryTarget(
             name: "MobilePaymentSDK",
             path: "Frameworks/MobilePaymentSDK.xcframework"
